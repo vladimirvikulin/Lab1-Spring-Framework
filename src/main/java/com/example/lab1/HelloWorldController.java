9cef968f2164eecf191ba70d1fba897cf0a21228
danil2205 <04danil@gmail.com>
2023-09-20 13:31:36 +0300

    added: empty controller

---
+++ src/main/java/com/example/lab1/HelloWorldController.java
@@ -0,0 +1,10 @@
+package com.example.lab1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloWorldController {
+}
