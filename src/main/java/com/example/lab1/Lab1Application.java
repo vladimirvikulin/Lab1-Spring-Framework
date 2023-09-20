17f7c57843d32210653a1043242200056b238973
vladimirvikulin <v5o4v7a9@gmail.com>
2023-09-20 10:55:44 +0300

    added Hello World console application

---
+++ src/main/java/com/example/lab1/Lab1Application.java
@@ -0,0 +1,18 @@
+package com.example.lab1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello from Spring Boot");
+	}
+}
